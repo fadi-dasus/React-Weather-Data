0085a8089785a8ad853e6f359d9cf15557affcd3
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-10-30 19:22:08 +0100

    weather data server added to the project and the configuration file has been updated

--- weather-data/src/components/App.js
+++ weather-data/src/components/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import HomePage from "./HomePage";
-// import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-// import CoursesPage from "./CoursesPage";
-// import ManageCoursePage from "./ManageCoursePage";
+
 import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import { ToastContainer } from "react-toastify";
@@ -16,10 +14,6 @@ function App() {
             <Header />
             <Switch>
                 <Route path="/" exact component={HomePage} />
-                {/* <Route path="/courses" component={CoursesPage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/course/:slug" component={ManageCoursePage} />
-                <Route path="/course" component={ManageCoursePage} /> */}
                 <Redirect from="/about-page" to="about" />
                 <Route component={NotFoundPage} />
             </Switch>
